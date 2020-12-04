fb7cb079ef0e4cfafcae5c3577957d38e7625581
Amit Kumar Gupta <akgmt20@gmail.com>
2020-12-05 00:25:01 +0530

    changed

--- src/main/java/k8sDemo/test/Test.java
+++
@@ -1,8 +0,0 @@
-package k8sDemo.test;
-
-public class Test {
-    public static void main(String[] args) {
-        new Test();
-        System.out.println("Hello World! Welcome to 2020");
-    }
-}
